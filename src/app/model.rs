66aa4ae349828e411c732596a0a3aa335a6a6077
Matthew Nicholson <matt@matt-land.com>
2020-01-06 23:20:26 -0500

    add simple_update() method to Update trait
    This allows the user to ignore commands if desired.

--- src/app/model.rs
+++ src/app/model.rs
@@ -4,8 +4,15 @@ use crate::app::side_effect::Commands;
 
 /// Process a message that updates the model.
 pub trait Update<Message, Command = ()> {
-    /// Update the model using the given message.
-    fn update(&mut self, msg: Message, commands: &mut Commands<Command>);
+    /// Update the model using the given message. Implement this to describe the behavior of your
+    /// app.
+    fn update(&mut self, msg: Message, _commands: &mut Commands<Command>) {
+        self.simple_update(msg);
+    }
+
+    /// Update the model using the given message. Implement this if your app does not need to use
+    /// side effecting commands.
+    fn simple_update(&mut self, _msg: Message) { }
 }
 
 /// Render (or view) the model as a virtual dom.
